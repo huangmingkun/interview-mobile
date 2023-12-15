ca8be0035c8fa2649e95b2c82f1e49f9e1292883
andun <1198966495@qq.com>
2023-12-15 14:14:01 +0800

    路由传参

---
+++ src/views/router-params/pageA.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "pageA"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/router-params/pageB.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "pageB"
+}
+</script>
+
+<style scoped>
+
+</style>
