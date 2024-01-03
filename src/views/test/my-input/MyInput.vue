9ef0310381f7c6f1b1eeb11653e6d43f4f0ac17e
andun <1198966495@qq.com>
2024-01-03 14:23:19 +0800

    封装基础input组件，添加防抖

---
+++ src/views/test/my-input/MyInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-input">
+    <label>{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :type="type"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyInput',
+  props: {
+    type: {
+      type: String,
+      required: false
+    },
+    label: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/test/my-input/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <my-input v-model="inputVal" label="姓名" />
+  </div>
+</template>
+
+<script>
+import MyInput from './MyInput.vue'
+export default {
+  name: 'index',
+  data () {
+    return {
+      inputVal: '111'
+    }
+  },
+  components: {
+    MyInput
+  },
+  watch: {
+    inputVal (newVal, oldVal) {
+      // console.log('新旧值', { newVal, oldVal })
+      this.getDataByInputVal()
+    }
+  },
+  created () {
+    this.getDataByInputVal = this.debounce(this.getDataByInputVal, 1000)
+  },
+  methods: {
+    debounce (fn, wait = 500) {
+      let timer = null
+      return function (...args) {
+        timer && clearTimeout(timer)
+        timer = setTimeout(() => {
+          console.log('防抖执行了')
+          fn()
+        }, wait)
+      }
+    },
+    getDataByInputVal () {
+      setTimeout(() => {
+        console.log(this.inputVal)
+      }, 1000)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
